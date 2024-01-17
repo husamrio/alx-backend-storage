3d11a8e5124c269d83f03058b6efc1e4b36ceffc
husamrio <zulfiqrd@gmail.com>
2024-01-17 20:50:23 +0300

    We are all unique!

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- Write a SQL script that creates a table users
+-- Makes table with unique users
+
+CREATE TABLE IF NOT EXISTS users (
+    id int NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
